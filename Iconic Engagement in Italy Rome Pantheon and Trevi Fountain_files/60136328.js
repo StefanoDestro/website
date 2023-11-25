c52551ee1caaa145df1b480975f9b85b98834f6c
Stefano <dst.sfn@gmail.com>
2023-11-26 00:53:20 +0100

    Commit

---
+++ Iconic Engagement in Italy Rome Pantheon and Trevi Fountain_files/60136328.js
@@ -0,0 +1,12 @@
+_iub.csRC = { hasCookiePolicy: true, hasPrivacyPolicy: true }
+_iub.csEnabled = true;
+_iub.csPurposes = [1,"s"];
+_iub.ppUpd = 1689329876;
+_iub.csFeatures = {
+  geolocation_setting: true,
+  cookie_solution_white_labeling: 0,
+  rejection_recovery: false,
+  full_customization: true,
+  multiple_languages: "en"
+};
+_iub.csT = null;
